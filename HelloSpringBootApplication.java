368fc03ecccfd8f1be0026c49d13564da1f21777
shivangib96 <shivangi.behera96@gmail.com>
2020-01-17 13:30:36 +0530

    Add files via upload

---
+++ HelloSpringBootApplication.java
@@ -0,0 +1,16 @@
+package boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringBootApplication {
+
+	public static void main(String[] args) {
+			SpringApplication.run(HelloSpringBootApplication.class,args);
+
+	}	
+
+	}
+
+
